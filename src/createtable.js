fded807fc324c996ce6cdd4ce19581f07b88cdeb
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-02-03 15:23:27 +0300

    добавлен файл стилей для печати. приложение работает: смена страниц, смена месяцев и годов, вывод на печать

--- src/createtable.js
+++ src/createtable.js
@@ -1,27 +1,35 @@
-import {tables, columns, currentPage, monthDuration} from './config.json';
+import {tables, columns, defaultPage, monthDuration} from './config.json';
 import {CreateCell, CreateCellNamed} from './createcell.js';
 
-
 export class CreateTable {
-    constructor(currentMonth=false) {
+    constructor(currentPage=defaultPage) {
         this.placeForStylize=document.querySelector('.allthereis');
         this.date=new Date();
-        this.numOfRows=Number(monthDuration[this.date.getMonth()][0]);
-        if (currentMonth) {this.numOfRows=Number(currentMonth)}
-        this.numOfColumns=tables[currentPage].length*columns.length;
         this.numOfRowsIndent=3; /* столько строк добавим на заголовки таблицы */
         this.IndentX=2; /* один столбец для даты, поэтому начинаем с 2 */
-        this.dateWidth=`10vh`; /* колонка с датами */
+        this.dateWidth=`15vh`; /* колонка с датами */
         this.yearHeight=`4vh`; /* заголовок года */
         this.packetHeight=`4vh`; /* заголовок пакета */
         this.parameterHeight=`4vh`; /* заголовок параметра */
 
+        this.currentMonth=this.date.getMonth(); /* месяц по дефолту вначале */
+        this.currentYear=this.date.getFullYear(); /* год по дефолту вначале */
+        this.currentPage=currentPage; /* по дефолту страница из конфига */
+
+        this.numOfRows=Number(monthDuration[this.currentMonth][0]); /* количество строк в зависимости от длины месяца берется из конфига */
+        this.numOfColumns=tables[this.currentPage].length*columns.length; /* количество столбцов берется из конфига для каждой страницы */
+
+        this.clearPage();
         this.createGridStyle();
         this.createTableSimple();
         this.createTableHead();
         this.createTableDates();
-
     }
+
+    clearPage() {
+        this.placeForStylize.innerHTML='';
+    }
+
     createGridStyle() { /* разметка грида */
 
         this.placeForStylize.style.gridTemplateColumns=`[beginDate beginYear] ${this.dateWidth} [endDate] repeat(${this.numOfColumns}, 1fr) [endYear]`;
@@ -30,6 +38,7 @@ export class CreateTable {
         ${this.parameterHeight} [endParameter] repeat(${this.numOfRows}, 1fr)
         `;
     }
+
     createTableSimple() { /* создание множества пустых ячеек */
         for (let j=1+this.numOfRowsIndent; j <= this.numOfRows+this.numOfRowsIndent; j++) {
             for (let i=this.IndentX; i < this.numOfColumns+this.IndentX; i++) {
@@ -37,19 +46,20 @@ export class CreateTable {
             }
         }
     }
+
     createTableHead(){
         /*** заголовок года ***/
         new CreateCellNamed('beginYear','endYear','beginYear','endYear',
-            `${this.date.toLocaleString('ru', {month: 'long'})} ${this.date.getFullYear()} года`);
+            `${monthDuration[this.currentMonth][2]} ${this.currentYear} года`); // тут подумать!!!
 
         /*** заголовок пакета ***/
-        tables[currentPage].forEach(function(el,ind){
+        tables[this.currentPage].forEach(function(el,ind){
 
             new CreateCellNamed(ind*2+this.IndentX, ind*2+this.IndentX+2,'beginPacket','endPacket',el)
         },this);
 
         /*** заголовок параметра ***/
-        tables[currentPage].forEach(function(el,ind){
+        tables[this.currentPage].forEach(function(el,ind){
             const indexOuter=ind;
 
             columns.forEach(function(el,ind){
@@ -61,11 +71,35 @@ export class CreateTable {
         new CreateCellNamed(1,2,'beginPacket','endParameter','Дата:');
 
     }
+
     createTableDates(){/* создание множества ячеек с датами */
         for (let j=1+this.numOfRowsIndent; j <= this.numOfRows+this.numOfRowsIndent; j++) {
 
-                new CreateCell(1,j,`${j-this.numOfRowsIndent} ${monthDuration[this.date.getMonth()][1]}`);
+                new CreateCell(1,j,`${j-this.numOfRowsIndent} ${monthDuration[this.currentMonth][1]}`);
         }
     }
+
+    updatePage(page=false,month=0) {
+
+        if (page) {this.currentPage++; if (this.currentPage>=tables.length) {this.currentPage=0}} /* циклическая смена страницы */
+
+        this.currentMonth=this.currentMonth+month;
+        if (this.currentMonth<0) {this.currentMonth=11; this.currentYear--}; /* переход года назад */
+        if (this.currentMonth>11) {this.currentMonth=0; this.currentYear++}; /* переход года вперед */
+
+        this.updateLayout();
+        this.clearPage();
+        this.createGridStyle();
+        this.createTableSimple();
+        this.createTableHead();
+        this.createTableDates();
+    }
+
+    updateLayout() {
+        this.numOfRows=Number(monthDuration[this.currentMonth][0]); /* перерасчет строк */
+        this.numOfColumns=tables[this.currentPage].length*columns.length; /* перерасчет столбцов */
+
+    }
+
 }
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import {CreateTable} from './createtable.js';
 
 
 
-new CreateTable();
+const journalPage=new CreateTable();
+
+document.querySelector('.changePage').addEventListener('click', ()=>{journalPage.updatePage(true,false,this)});
+
+document.querySelector('.changeMonthForward').addEventListener('click', ()=>{journalPage.updatePage(false,1,this)});
+
+document.querySelector('.changeMonthBackward').addEventListener('click', ()=>{journalPage.updatePage(false,-1,this)});
+
+document.querySelector('.printPage').addEventListener('click', ()=>window.print());
