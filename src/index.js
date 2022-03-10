800c0a61e4218a7444af37767136b86c333bc283
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-03-10 18:40:27 +0300

    в основном скрипте основные действия теперь срабатывают и по клавишам

--- src/index.js
+++ src/index.js
@@ -14,4 +14,13 @@ document.querySelector('.printPage').addEventListener('click', ()=>window.print(
 document.querySelector('.changeJournal').addEventListener('click', ()=>{journal.changeJournal(this)});
 
 
-
+/* добавление клавиш основных действий: */
+document.addEventListener('keyup',()=>{
+    switch (event.key) {
+        case 'p': window.print(); break; /* [P]rint page */
+        case 'c': journal.changeJournal(this); break; /* [C]hange journal */
+        case 'l': journal.updatePage(true,false,this); break; /* [L]ist page */
+        case '+': journal.updatePage(false,1,this); break; /* month [+] */
+        case '-': journal.updatePage(false,-1,this); break; /* month [-] */
+    }
+});
